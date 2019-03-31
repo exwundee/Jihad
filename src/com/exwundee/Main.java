e8f4ea17b4f2a1d739b3dfb984e2b16a88f84ee7
Nicholas <36908791+exwundee@users.noreply.github.com>
2019-03-31 09:28:14 -0400

    sent wrong directory

--- src/com/exwundee/Main.java
+++ src/com/exwundee/Main.java
@@ -1,9 +1,6 @@
 package com.exwundee;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
@@ -13,7 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -35,6 +32,75 @@ public class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
         System.out.println("§aJihad created by x1D enabled.");
+
+        getConfig().addDefault("delay", 120);
+        getConfig().addDefault("world", "world");
+        getConfig().options().copyDefaults(true);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                int d = rand.nextInt(8);
+                try {
+                    ItemStack[] is = {new ItemStack(Material.END_CRYSTAL, 1), new ItemStack(Material.DIAMOND_SWORD, 1),
+                            new ItemStack(Material.DIAMOND_PICKAXE, 1), new ItemStack(Material.BEDROCK, 1),
+                            new ItemStack(Material.TNT, 1), new ItemStack(Material.OBSIDIAN, 1),
+                            new ItemStack(Material.BEACON, 1),
+                            new ItemStack(Material.TOTEM, 1), new ItemStack(Material.ENDER_PORTAL_FRAME, 1),
+                            new ItemStack(Material.EYE_OF_ENDER, 1)};
+
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        ItemStack ni = is[d];
+                        ItemMeta im = ni.getItemMeta();
+                        if (ni.getType() == Material.DIAMOND_SWORD) {
+                            im.addEnchant(Enchantment.DAMAGE_ALL, 32767, true);
+                            im.addEnchant(Enchantment.KNOCKBACK, 10, true);
+                            im.addEnchant(Enchantment.FIRE_ASPECT, 32767, true);
+                            im.addEnchant(Enchantment.LOOT_BONUS_MOBS, 10, true);
+                            im.addEnchant(Enchantment.DURABILITY, 32767, true);
+                            im.addEnchant(Enchantment.MENDING, 1, true);
+                            im.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+                            ni.setItemMeta(im);
+                        }
+                        if (ni.getType() == Material.DIAMOND_PICKAXE) {
+                            im.addEnchant(Enchantment.DIG_SPEED, 32767, true);
+                            im.addEnchant(Enchantment.SILK_TOUCH, 10, true);
+                            im.addEnchant(Enchantment.DURABILITY, 32767, true);
+                            im.addEnchant(Enchantment.MENDING, 1, true);
+                            im.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+                            ni.setItemMeta(im);
+                        }
+                        p.getInventory().addItem(is[d]);
+                    }
+                    int e = rand.nextInt(8);
+                    ItemStack ni = is[e];
+                    ItemMeta im = ni.getItemMeta();
+                    if (ni.getType() == Material.DIAMOND_SWORD) {
+                        im.addEnchant(Enchantment.DAMAGE_ALL, 32767, true);
+                        im.addEnchant(Enchantment.KNOCKBACK, 10, true);
+                        im.addEnchant(Enchantment.FIRE_ASPECT, 32767, true);
+                        im.addEnchant(Enchantment.LOOT_BONUS_MOBS, 10, true);
+                        im.addEnchant(Enchantment.DURABILITY, 32767, true);
+                        im.addEnchant(Enchantment.MENDING, 1, true);
+                        im.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+                        ni.setItemMeta(im);
+                    }
+                    if (ni.getType() == Material.DIAMOND_PICKAXE) {
+                        im.addEnchant(Enchantment.DIG_SPEED, 32767, true);
+                        im.addEnchant(Enchantment.SILK_TOUCH, 10, true);
+                        im.addEnchant(Enchantment.DURABILITY, 32767, true);
+                        im.addEnchant(Enchantment.MENDING, 1, true);
+                        im.addEnchant(Enchantment.VANISHING_CURSE, 1, true);
+                        ni.setItemMeta(im);
+                    }
+                    Bukkit.getWorld(getConfig().getString("world")).dropItemNaturally(new Location(Bukkit.getWorld(getConfig().getString("world")), 0,
+                            Bukkit.getWorld(getConfig().getString("world")).getHighestBlockYAt(0, 0) + 1, 0), is[e]);
+                } catch (Exception e) {
+
+                }
+            }
+        }, 1L, 120*20);
+
     }
 
     @Override
@@ -126,14 +192,39 @@ public class Main extends JavaPlugin implements Listener {
     }
 
     @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        String msg = event.getMessage().replaceAll("fuck", "flubbernugget")
+                .replaceAll("shit", "shart")
+                .replaceAll("nigger", "John200410")
+                .replaceAll("ass", "#*@()!")
+                .replaceAll("hell", "4b4t")
+                .replaceAll("bitch", "lAsAgnA")
+                .replaceAll("cunt", "kitty kat")
+                .replaceAll("damn", "DANIEL")
+                .replaceAll("nigga", "John2OO41O")
+                .replaceAll("prick", "x1D")
+                .replaceAll("twat", "britfag")
+                .replaceAll("hitler", "jew")
+                .replaceAll("hause", "h00semustard")
+                .replaceAll("2b2t", "bad server")
+                .replaceAll("rusherhack", "§c§lrusherhack§r")
+                .replaceAll("minecraft", " ")
+                .replaceAll("no u", "yes me")
+                .replaceAll("6b9t", "ABSOLUTE GARBAGE")
+                .replaceAll("discord", "b&");
+        event.setMessage(msg + " >>> buy rusherhack from John200410#0353 on discord https://discord.gg/RmDrJ4a");
+    }
+
+    @EventHandler
     public void onPlace(BlockPlaceEvent event) {
         if (event.getBlock().getType() == Material.TNT) {
             int a = rand.nextInt(200);
             if (a == 69) event.getBlock().setType(Material.BEDROCK);
 
-            Location l = event.getPlayer().getLocation();
+            Location l = event.getBlock().getLocation();
 
-            if (event.getPlayer().getWorld().getBlockAt((int) l.getX(), (int) l.getY() - 1, (int) l.getZ()).getType() == Material.BEDROCK) {
+            if (event.getPlayer().getWorld().getBlockAt((int) l.getX(), (int) l.getY() - 1, (int) l.getZ()).getType() == Material.BEDROCK ||
+                    event.getPlayer().getWorld().getBlockAt((int) l.getX(), (int) l.getY() - 1, (int) l.getZ()).getType() == Material.OBSIDIAN) {
                 event.getBlock().setType(Material.AIR);
                 event.getPlayer().getWorld().spawnEntity(l, EntityType.PRIMED_TNT);
             }
